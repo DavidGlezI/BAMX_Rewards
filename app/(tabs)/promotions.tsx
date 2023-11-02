2b75c96bd7afd62215d40f620653fe37ff7e9c41
Riku-li <rickyweringo6@gmail.com>
2023-11-02 07:56:53 -0600

    Added rectangles

--- app/(tabs)/promotions.tsx
+++ app/(tabs)/promotions.tsx
@@ -1,21 +1,52 @@
 import React from "react";
-import { StyleSheet, View, Text, Image} from "react-native";
-import { ScrollView } from "react-native-gesture-handler";
+import { StyleSheet, View, Text, Image, ScrollView } from "react-native";
 
 const images = [
-  require ("../../assets/images/QinLogo.png"),
-  require ("../../assets/images/CarlsLogo.png"),
-  require ("../../assets/images/QinLogo.png"),
-  require ("../../assets/images/QinLogo.png"),
-  require ("../../assets/images/CarlsLogo.png"),
-  require ("../../assets/images/QinLogo.png"),
-  require ("../../assets/images/CarlsLogo.png")
+  require("../../assets/images/QinLogo.png"),
+  require("../../assets/images/CarlsLogo.png"),
+  require("../../assets/images/QinLogo.png"),
+  require("../../assets/images/QinLogo.png"),
+  require("../../assets/images/CarlsLogo.png"),
+  require("../../assets/images/QinLogo.png"),
+  require("../../assets/images/CarlsLogo.png"),
 ];
+
+const rectangles = [
+  {
+    id: "1",
+    text: "Promoción 1",
+    image: require("../../assets/images/QinLogo.png"),
+  },
+  {
+    id: "2",
+    text: "Promoción 2",
+    image: require("../../assets/images/QinLogo.png"),
+  },
+  {
+    id: "3",
+    text: "Promoción 3",
+    image: require("../../assets/images/QinLogo.png"),
+  },
+  {
+    id: "4",
+    text: "Promoción 4",
+    image: require("../../assets/images/QinLogo.png"),
+  },
+  {
+    id: "5",
+    text: "Promoción 5",
+    image: require("../../assets/images/QinLogo.png"),
+  },
+];
+
 export default function TabPromotionsScreen() {
+  const row1 = rectangles.filter((_, index) => index % 2 === 0);
+  const row2 = rectangles.filter((_, index) => index % 2 === 1);
+
   return (
     <View style={styles.container}>
       <Text style={styles.establecimientosTitle}>ESTABLECIMIENTOS</Text>
-      <ScrollView  horizontal={true}>
+      <ScrollView horizontal={true} contentContainerStyle={styles.circlesContainer}>
         {images.map((image, index) => (
           <View key={index} style={styles.circle}>
             <Image source={image} style={styles.image} />
@@ -23,6 +54,24 @@ export default function TabPromotionsScreen() {
         ))}
       </ScrollView>
       <Text style={styles.promocionesTitle}>PROMOCIONES</Text>
+      <ScrollView horizontal={true} contentContainerStyle={styles.rectanglesContainer}>
+        <View style={styles.row}>
+          {row1.map((item) => (
+            <View key={item.id} style={styles.rectangle}>
+              <Image source={item.image} style={styles.rectangleImage} />
+              <Text style={styles.rectangleText}>{item.text}</Text>
+            </View>
+          ))}
+        </View>
+        <View style={styles.row}>
+          {row2.map((item) => (
+            <View key={item.id} style={styles.rectangle}>
+              <Image source={item.image} style={styles.rectangleImage} />
+              <Text style={styles.rectangleText}>{item.text}</Text>
+            </View>
+          ))}
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -55,12 +104,10 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-around",
   },
-
   circle: {
     width: 58,
     height: 58,
-    borderRadius: 50,
-    padding: 2,
+    borderRadius: 29,
     backgroundColor: "#FFF",
     flexShrink: 0,
     margin: 10,
@@ -72,13 +119,44 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.23,
     shadowRadius: 2.62,
     elevation: 4,
-    overflow: "hidden", 
+    overflow: "hidden",
   },
   image: {
     width: "100%",
     height: "100%",
-    borderRadius: 24,
-    objectFit: "contain",
-    padding: 5,
+  },
+  rectanglesContainer: {
+    flexDirection: 'row',
+  },
+  row: {
+    flexDirection: 'column',
+  },
+  rectangle: {
+    backgroundColor: "#FFF",
+    borderRadius: 10,
+    padding: 10,
+    margin: 5,
+    width: 100,
+    alignItems: "center",
+    justifyContent: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+    elevation: 4,
+  },
+  rectangleImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    marginBottom: 10,
+  },
+  rectangleText: {
+    fontSize: 16,
+    color: "#000",
+    textAlign: "center",
   },
 });
